9ae6443e3b70e7d15671389217f5e095f05ccd8d
k5rv <k.saraev@gmail.com>
2023-03-09 07:21:26 +0200

    Update configuration

--- src/main/java/com/ksaraev/spotifyrun/config/requests/GetRecommendationsRequestConfig.java
+++ src/main/java/com/ksaraev/spotifyrun/config/requests/GetRecommendationsRequestConfig.java
@@ -1,11 +1,11 @@
 package com.ksaraev.spotifyrun.config.requests;
 
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
+@Builder
 public class GetRecommendationsRequestConfig implements SpotifyGetRecommendationsRequestConfig {
 
-  @Value("${spotifyrun.config.client.requests.get-recommendations.limit}")
   private Integer limit;
 }
