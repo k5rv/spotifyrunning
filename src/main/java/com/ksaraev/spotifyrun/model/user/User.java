2b5ce5534daa3469957754ad9c829b2adc153678
k5rv <k.saraev@gmail.com>
2023-03-16 07:32:15 +0200

    Add postgresql

--- src/main/java/com/ksaraev/spotifyrun/model/user/User.java
+++ src/main/java/com/ksaraev/spotifyrun/model/user/User.java
@@ -1,17 +1,24 @@
 package com.ksaraev.spotifyrun.model.user;
 
 import com.ksaraev.spotifyrun.model.spotify.SpotifyUser;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.net.URI;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Entity
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "spotify_user")
 public class User implements SpotifyUser {
-  @NotNull private String id;
+  @Id @NotNull private String id;
   @NotEmpty private String name;
   @Email private String email;
   @NotNull private URI uri;
