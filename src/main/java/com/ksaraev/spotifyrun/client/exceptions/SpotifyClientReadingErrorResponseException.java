fe16fc255d80139ae5d7bf412d41eeb905d2fed9
k5rv <k.saraev@gmail.com>
2023-04-03 09:19:45 +0300

    Update client

--- src/main/java/com/ksaraev/spotifyrun/client/exceptions/SpotifyClientReadingErrorResponseException.java
+++ src/main/java/com/ksaraev/spotifyrun/client/exceptions/SpotifyClientReadingErrorResponseException.java
@@ -4,5 +4,6 @@ import lombok.experimental.StandardException;
 
 @StandardException
 public class SpotifyClientReadingErrorResponseException extends SpotifyClientException {
-  public static final String UNABLE_TO_READ_ERROR_RESPONSE = "Unable to read Spotify API error: ";
+  public static final String UNABLE_TO_READ_ERROR_RESPONSE =
+          "Unable to read Spotify API error: ";
 }
