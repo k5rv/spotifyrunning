d4bf75ba9d1d97984aedbd1a9ddf25b46d65f734
k5rv <k.saraev@gmail.com>
2023-04-13 08:14:03 +0300

    Update security config

--- src/main/java/com/ksaraev/spotifyrun/security/AppSecurityConfig.java
+++ src/main/java/com/ksaraev/spotifyrun/security/AppSecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile(value = {"development", "production"})
+@Profile(value = {"development", "production", "docker"})
 public class AppSecurityConfig {
 
   @Bean
