c12a823c285a999b6b0f78d315088883ad9082e6
k5rv <k.saraev@gmail.com>
2023-05-14 16:59:25 +0300

    Update front-end user registration

---
+++ src/main/resources/static/http-client.js
@@ -0,0 +1,27 @@
+// const BASE_URL = "http://localhost:8082"
+// const API_V1_USERS = "/api/v1/users"
+
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
