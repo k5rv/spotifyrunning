883db8454b448c76bbdb09dfb3f7830c27bc171d
k5rv <k.saraev@gmail.com>
2023-05-15 07:15:02 +0300

    Add js http client

--- src/main/resources/static/http-client.js
+++ src/main/resources/static/http-client.js
@@ -1,9 +1,6 @@
-// const BASE_URL = "http://localhost:8082"
-// const API_V1_USERS = "/api/v1/users"
-
 function makeRequest(method, url) {
     return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open(method, url);
         xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 300) {
