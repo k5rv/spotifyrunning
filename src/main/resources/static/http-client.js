c43e2bf7be2b5d90e3d5a68109e9bde66282ac1c
k5rv <k.saraev@gmail.com>
2023-05-16 07:36:39 +0300

    Update http-client.js

--- src/main/resources/static/http-client.js
+++ src/main/resources/static/http-client.js
@@ -3,14 +3,14 @@ function makeRequest(method, url) {
         const xhr = new XMLHttpRequest();
         xhr.open(method, url);
         xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
+            // if (xhr.status >= 200 && xhr.status < 300) {
                 resolve(xhr.response);
-            } else {
-                reject({
-                    status: xhr.status,
-                    statusText: xhr.statusText
-                });
-            }
+            // } else {
+            //     reject({
+            //         status: xhr.status,
+            //         statusText: xhr.statusText
+            //     });
+            // }
         };
         xhr.onerror = function () {
             reject({
