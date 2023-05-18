44a7d1d4d548594c6b33eb39660e419f75fd4e33
k5rv <k.saraev@gmail.com>
2023-05-18 19:47:33 +0300

    Create SpotifyPlaylistServiceCreatePlaylistTest

--- src/test/java/com/suddenrun/spotify/service/playlist/SpotifyPlaylistServiceCreatePlaylistTest.java
+++ src/test/java/com/suddenrun/spotify/service/playlist/SpotifyPlaylistServiceCreatePlaylistTest.java
@@ -1,3 +1,192 @@
 package com.suddenrun.spotify.service.playlist;
 
-public class SpotifyPlaylistServiceCreatePlaylistTest {}
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import com.suddenrun.spotify.client.SpotifyClient;
+import com.suddenrun.spotify.client.dto.SpotifyPlaylistDetailsDto;
+import com.suddenrun.spotify.client.dto.SpotifyPlaylistDto;
+import com.suddenrun.spotify.config.AddSpotifyPlaylistItemsRequestConfig;
+import com.suddenrun.spotify.exception.CreateSpotifyPlaylistException;
+import com.suddenrun.spotify.model.playlist.SpotifyPlaylist;
+import com.suddenrun.spotify.model.playlist.SpotifyPlaylistMapper;
+import com.suddenrun.spotify.model.playlistdetails.SpotifyPlaylistDetails;
+import com.suddenrun.spotify.model.playlistdetails.SpotifyPlaylistItemDetails;
+import com.suddenrun.spotify.model.userprofile.SpotifyUserProfileItem;
+import com.suddenrun.spotify.service.SpotifyPlaylistItemService;
+import com.suddenrun.spotify.service.SpotifyPlaylistService;
+import com.suddenrun.utils.helpers.SpotifyClientHelper;
+import com.suddenrun.utils.helpers.SpotifyServiceHelper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.executable.ExecutableValidator;
+import java.lang.reflect.Method;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class SpotifyPlaylistServiceCreatePlaylistTest {
+
+  private static final String CREATE_PLAYLIST = "createPlaylist";
+
+  private static final ExecutableValidator executableValidator =
+      Validation.buildDefaultValidatorFactory().getValidator().forExecutables();
+
+  @Mock private SpotifyClient client;
+
+  @Mock private AddSpotifyPlaylistItemsRequestConfig requestConfig;
+
+  @Mock private SpotifyPlaylistMapper mapper;
+
+  @Captor private ArgumentCaptor<String> userIdArgumentCaptor;
+
+  @Captor private ArgumentCaptor<SpotifyPlaylistDetails> playlistDetailsArgumentCaptor;
+
+  @Captor private ArgumentCaptor<SpotifyPlaylistDto> playlistDtoArgumentCaptor;
+
+  @Captor private ArgumentCaptor<SpotifyPlaylistDetailsDto> playlistDetailsDtoArgumentCaptor;
+
+  private SpotifyPlaylistItemService underTest;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    underTest = new SpotifyPlaylistService(client, requestConfig, mapper);
+  }
+
+  @Test
+  void itShouldCreatePlaylist() {
+    // Given
+    SpotifyPlaylistDetailsDto playlistDetailsDto = SpotifyClientHelper.getPlaylistDetailsDto();
+    SpotifyPlaylistDto playlistDto = SpotifyClientHelper.getPlaylistDto();
+    SpotifyUserProfileItem userProfile = SpotifyServiceHelper.getUserProfile();
+    SpotifyPlaylistDetails playlistDetails =
+        (SpotifyPlaylistDetails) SpotifyServiceHelper.getPlaylistDetails();
+    SpotifyPlaylist playlist = (SpotifyPlaylist) SpotifyServiceHelper.getPlaylist();
+    given(mapper.mapToPlaylistDetailsDto(any(SpotifyPlaylistItemDetails.class)))
+        .willReturn(playlistDetailsDto);
+    given(client.createPlaylist(any(), any())).willReturn(playlistDto);
+    given(mapper.mapToModel(any(SpotifyPlaylistDto.class))).willReturn(playlist);
+    // When
+    underTest.createPlaylist(userProfile, playlistDetails);
+    // Then
+    then(mapper).should().mapToPlaylistDetailsDto(playlistDetailsArgumentCaptor.capture());
+    assertThat(playlistDetailsArgumentCaptor.getValue())
+        .isNotNull()
+        .usingRecursiveComparison()
+        .isEqualTo(playlistDetails);
+    then(client)
+        .should()
+        .createPlaylist(userIdArgumentCaptor.capture(), playlistDetailsDtoArgumentCaptor.capture());
+    assertThat(userIdArgumentCaptor.getValue()).isNotNull().isEqualTo(userProfile.getId());
+    assertThat(playlistDetailsDtoArgumentCaptor.getValue())
+        .isNotNull()
+        .usingRecursiveComparison()
+        .isEqualTo(playlistDetailsDto);
+    then(mapper).should().mapToModel(playlistDtoArgumentCaptor.capture());
+    assertThat(playlistDtoArgumentCaptor.getValue())
+        .isNotNull()
+        .usingRecursiveComparison()
+        .isEqualTo(playlistDto);
+  }
+
+  @Test
+  void itShouldThrowCreatePlaylistExceptionWhenPlaylistMapperThrowsRuntimeException() {
+    // Given
+    String message = "message";
+    SpotifyUserProfileItem spotifyUser = SpotifyServiceHelper.getUserProfile();
+    String userId = spotifyUser.getId();
+    SpotifyPlaylistItemDetails spotifyPlaylistDetails = SpotifyServiceHelper.getPlaylistDetails();
+    given(mapper.mapToPlaylistDetailsDto(any(SpotifyPlaylistItemDetails.class)))
+        .willThrow(new RuntimeException(message));
+    // Then
+    assertThatThrownBy(() -> underTest.createPlaylist(spotifyUser, spotifyPlaylistDetails))
+        .isExactlyInstanceOf(CreateSpotifyPlaylistException.class)
+        .hasMessageContaining(userId)
+        .hasMessageContaining(message);
+  }
+
+  @Test
+  void itShouldDetectCreatePlaylistConstraintViolationsWhenSpotifyUserProfileIsNull()
+      throws Exception {
+    // Given
+    SpotifyPlaylistItemDetails playlistDetails = SpotifyServiceHelper.getPlaylistDetails();
+    Method method =
+        SpotifyPlaylistService.class.getMethod(
+            CREATE_PLAYLIST, SpotifyUserProfileItem.class, SpotifyPlaylistItemDetails.class);
+    Object[] parameterValues = {null, playlistDetails};
+    // When
+    Set<ConstraintViolation<SpotifyPlaylistItemService>> constraintViolations =
+        executableValidator.validateParameters(underTest, method, parameterValues);
+    // Then
+    assertThat(constraintViolations).hasSize(1);
+    assertThat(new ConstraintViolationException(constraintViolations))
+        .hasMessage(CREATE_PLAYLIST + ".user: must not be null");
+  }
+
+  @Test
+  void itShouldDetectCreatePlaylistConstraintViolationsWhenSpotifyPlaylistItemDetailsIsNull()
+      throws Exception {
+    // Given
+    SpotifyUserProfileItem userProfile = SpotifyServiceHelper.getUserProfile();
+    Method method =
+        SpotifyPlaylistService.class.getMethod(
+            CREATE_PLAYLIST, SpotifyUserProfileItem.class, SpotifyPlaylistItemDetails.class);
+    Object[] parameterValues = {userProfile, null};
+    // When
+    Set<ConstraintViolation<SpotifyPlaylistItemService>> constraintViolations =
+        executableValidator.validateParameters(underTest, method, parameterValues);
+    // Then
+    assertThat(constraintViolations).hasSize(1);
+    assertThat(new ConstraintViolationException(constraintViolations))
+        .hasMessage(CREATE_PLAYLIST + ".playlistItemDetails: must not be null");
+  }
+
+  @Test
+  void itShouldDetectCreatePlaylistCascadeConstraintViolationsWhenSpotifyUserIsNotValid()
+      throws Exception {
+    // Given
+    SpotifyUserProfileItem user = SpotifyServiceHelper.getUserProfile();
+    user.setId(null);
+    Method method =
+        SpotifyPlaylistService.class.getMethod(
+            CREATE_PLAYLIST, SpotifyUserProfileItem.class, SpotifyPlaylistItemDetails.class);
+    SpotifyPlaylistItemDetails playlistDetails = SpotifyServiceHelper.getPlaylistDetails();
+    Object[] parameterValues = {user, playlistDetails};
+    // When
+    Set<ConstraintViolation<SpotifyPlaylistItemService>> constraintViolations =
+        executableValidator.validateParameters(underTest, method, parameterValues);
+    // Then
+    assertThat(constraintViolations).hasSize(1);
+    assertThat(new ConstraintViolationException(constraintViolations))
+        .hasMessage(CREATE_PLAYLIST + ".user.id: must not be null");
+  }
+
+  @Test
+  void itShouldDetectCreatePlaylistCascadeConstraintViolationsWhenSpotifyPlaylistDetailsIsNotValid()
+      throws Exception {
+    // Given
+    SpotifyUserProfileItem user = SpotifyServiceHelper.getUserProfile();
+    SpotifyPlaylistItemDetails playlistDetails = SpotifyServiceHelper.getPlaylistDetails();
+    playlistDetails.setName(null);
+    Method method =
+        SpotifyPlaylistService.class.getMethod(
+            CREATE_PLAYLIST, SpotifyUserProfileItem.class, SpotifyPlaylistItemDetails.class);
+    Object[] parameterValues = {user, playlistDetails};
+    // When
+    Set<ConstraintViolation<SpotifyPlaylistItemService>> constraintViolations =
+        executableValidator.validateParameters(underTest, method, parameterValues);
+    // Then
+    assertThat(constraintViolations).hasSize(1);
+    assertThat(new ConstraintViolationException(constraintViolations))
+        .hasMessage(CREATE_PLAYLIST + ".playlistItemDetails.name: must not be empty");
+  }
+}
