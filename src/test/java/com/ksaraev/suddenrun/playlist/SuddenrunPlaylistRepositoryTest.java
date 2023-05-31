4d38dc09a2ad8c02c01546e25d3fec98d7335b74
k5rv <k.saraev@gmail.com>
2023-05-31 13:05:30 +0300

    Update SuddenrunPlaylistRepositoryTest

--- src/test/java/com/ksaraev/suddenrun/playlist/SuddenrunPlaylistRepositoryTest.java
+++ src/test/java/com/ksaraev/suddenrun/playlist/SuddenrunPlaylistRepositoryTest.java
@@ -3,6 +3,7 @@ package com.ksaraev.suddenrun.playlist;
 import static org.assertj.core.api.Assertions.*;
 
 import com.ksaraev.suddenrun.track.AppTrack;
+import com.ksaraev.suddenrun.track.SuddenrunTrack;
 import com.ksaraev.suddenrun.user.SuddenrunUser;
 import com.ksaraev.utils.helpers.SuddenrunHelper;
 import jakarta.persistence.EntityManager;
@@ -56,18 +57,36 @@ class SuddenrunPlaylistRepositoryTest {
   }
 
   @Test
-  void itShouldFindPlaylistAndItTracksById() {
+  void itShouldDetermineEqualityOfInstances() {
     // Given
-    SuddenrunUser user = SuddenrunHelper.getUser();
-    SuddenrunPlaylist playlist = SuddenrunHelper.getSuddenrunPlaylist(user);
-    String playlistId = playlist.getId();
-    List<AppTrack> tracks = SuddenrunHelper.getTracks(50);
-    playlist.setTracks(tracks);
-    playlist.setInclusions(List.of());
-    playlist.setInclusions(List.of());
-    user.setPlaylists(List.of(playlist));
-    entityManager.persist(user);
-    underTest.save(playlist);
+    String playlistId = "playlistIdA";
+
+    SuddenrunUser userA0 = SuddenrunUser.builder().id("userIdA").name("userA").build();
+    AppTrack trackA0 = SuddenrunTrack.builder().id("trackIdA").name("trackA").build();
+    AppTrack trackB0 = SuddenrunTrack.builder().id("trackIdB").name("trackB").build();
+    AppTrack trackC0 = SuddenrunTrack.builder().id("trackIdC").name("trackC").build();
+    SuddenrunPlaylist playlistA0 =
+        SuddenrunPlaylist.builder().id(playlistId).snapshotId("playlistIdASnapshot").build();
+    playlistA0.setUser(userA0);
+    playlistA0.setInclusions(List.of(trackC0));
+    playlistA0.setExclusions(List.of());
+    playlistA0.setTracks(List.of(trackA0, trackB0));
+    userA0.setPlaylists(List.of(playlistA0));
+
+    SuddenrunUser userA1 = SuddenrunUser.builder().id("userIdA").name("userA").build();
+    AppTrack trackA1 = SuddenrunTrack.builder().id("trackIdA").name("trackA").build();
+    AppTrack trackB1 = SuddenrunTrack.builder().id("trackIdB").name("trackB").build();
+    AppTrack trackC1 = SuddenrunTrack.builder().id("trackIdC").name("trackC").build();
+    SuddenrunPlaylist playlistA1 =
+        SuddenrunPlaylist.builder().id(playlistId).snapshotId("playlistIdASnapshot").build();
+    playlistA1.setUser(userA1);
+    playlistA1.setInclusions(List.of(trackC1));
+    playlistA1.setExclusions(List.of());
+    playlistA0.setTracks(List.of(trackA1, trackB1));
+    userA1.setPlaylists(List.of(playlistA1));
+
+    entityManager.persist(userA0);
+    underTest.save(playlistA0);
 
     // When
     Optional<SuddenrunPlaylist> optionalPlaylist = underTest.findById(playlistId);
@@ -75,11 +94,7 @@ class SuddenrunPlaylistRepositoryTest {
     // Then
     assertThat(optionalPlaylist)
         .isPresent()
-        .hasValueSatisfying(p -> assertThat(p.getTracks()).isEqualTo(tracks));
-    //    assertThat(optionalPlaylist)
-    //            .isPresent()
-    //            .hasValueSatisfying(p ->
-    // assertThat(p).usingRecursiveComparison().isEqualTo(playlist));
+        .hasValueSatisfying(p -> assertThat(p).isEqualTo(playlistA1));
   }
 
   @Test
